fa319f346c6626d9ef6228a178f1132c02c5f322
sainitishkumar <bsainitishkumar@gmail.com>
2020-07-13 18:30:12 +0530

    test

--- musicplayer/player/admin.py
+++ musicplayer/player/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Playlists, Song
 
-# Register your models here.
+admin.site.register(Playlists)
+admin.site.register(Song)
--- musicplayer/player/views.py
+++ musicplayer/player/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import Http404
+from .models import Playlists
+
+from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the player index.")
+    playlists = Playlists.objects.all()
+    context = {
+        'playlists': playlists,
+    }
+    return render(request, 'player/index.html', context)
+
+def get_playlist(request, playlist_id):
+    try:
+        plist = Playlists.objects.get(pk = playlist_id)
+    except Playlists.DoesNotExist:
+        raise Http404("no playlist with id: "+str(playlist_id))
+    return render(request, 'player/details.html', {'playlist': plist})
